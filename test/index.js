d28e55c36e4aa6ed463ee3f7c2f631b174de73cf
Shawn Inder <shawn@vigour.io>
2016-07-06 14:54:52 -0400

    Create assets in a temporary place and rename when done (closes #3)

--- test/index.js
+++ test/index.js
@@ -6,19 +6,29 @@ var test = require('tape')
 var Service = require('../')
 
 test('Service', function (t) {
-  t.plan(2)
+  t.plan(4)
   var service = new Service({
     url: fs.createReadStream(path.join(__dirname, 'data.json')),
     strategy: '@vigour-io/seo-mtv',
     dest: __dirname
   })
+  var replacingSitemapFired = false
+  var replacingJsonldsFired = false
+  service.on('replacingSitemap', () => {
+    replacingSitemapFired = true
+  })
   service.on('sitemap', (loc) => {
+    t.equal(replacingSitemapFired, true, '`replacingSitemap` fired before `sitemap`')
     fs.removeAsync(loc)
       .then(() => {
         t.equal(loc, path.join(__dirname, 'sitemap.xml'), "emits the produced sitemap's path")
       })
   })
+  service.on('replacingJsonlds', () => {
+    replacingJsonldsFired = true
+  })
   service.on('jsonlds', (dir) => {
+    t.equal(replacingJsonldsFired, true, '`replacingJsonlds` fired before `jsonlds`')
     fs.removeAsync(dir)
       .then(() => {
         t.equal(dir, path.join(__dirname, 'jsons'), 'emits the jsonld dir path')
